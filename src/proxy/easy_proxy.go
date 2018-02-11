e5b21e7dfb4f229633cb678a0816e7239832eb21
eric <liuwei2@digisky.com>
2018-02-11 11:58:18 +0800

    定义代理接口

--- src/proxy/easy_proxy.go
+++ src/proxy/easy_proxy.go
@@ -1,13 +1,41 @@
 package proxy
 
-import "config"
+import (
+	"config"
+	"net"
+)
 
 type EasyProxy struct {
 	data		*ProxyData
 }
 
-func (proxy EasyProxy)Init(config *config.Config)  {
+func (proxy *EasyProxy) Clean(url string) {
+	panic("implement me")
+}
+
+func (proxy *EasyProxy) Recover(url string) {
+	panic("implement me")
+}
+
+func (proxy *EasyProxy) Dispatch(con net.Conn) {
+	panic("implement me")
+}
+
+func (proxy *EasyProxy) Close() {
+	panic("implement me")
+}
+
+func (proxy *EasyProxy) Check()  {
+
+}
+
+func (proxy *EasyProxy)Init(config *config.Config)  {
 	proxy.data = new(ProxyData)
 	proxy.data.Init(config)
 }
+
+func (proxy *EasyProxy)Start()  {
+
+}
+
 
--- src/proxy/proxy.go
+++ src/proxy/proxy.go
@@ -1,8 +1,18 @@
 package proxy
 
-import "config"
+import (
+	"config"
+	"net"
+)
 
+// 代理接口
+// 定义了 初始化、检测、清理、恢复、转发、关闭
 type Proxy interface {
 	Init(config *config.Config)
+	Check()
+	Clean(url string)
+	Recover(url string)
+	Dispatch(con net.Conn)
+	Close()
 }
 
